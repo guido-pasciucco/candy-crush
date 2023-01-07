ec6094f99194a1451b1dbc07272440508eaaa1f2
Guido <guidopasciucco@gmail.com>
2023-01-07 11:29:49 -0300

    #3 added drag and drop functionality

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const rows = 9;
 const columns = 9;
 const score = 0;
 
+let currTile;
+let otherTile;
+
 window.onload = () => {
     startGame();
 }
@@ -19,6 +22,12 @@ const startGame = () => {
             let tile = document.createElement("img");
             tile.id = r.toString() + "-" + c.toString();
             tile.src = "./images/" + randomCandy() + ".png";
+            tile.addEventListener("dragstart", dragStart);
+            tile.addEventListener("dragover", dragOver);
+            tile.addEventListener("dragenter", dragEnter);
+            tile.addEventListener("dragleave", dragLeave);
+            tile.addEventListener("drop", dragDrop);
+            tile.addEventListener("dragend", dragEnd);
             document.getElementById("board").append(tile);
             row.push(tile);
         }
@@ -26,4 +35,32 @@ const startGame = () => {
     }
 }
 
-// drag and drop functionality
+// este commit : added a drag and drop functionality
+
+const dragStart = () => currTile = this;
+const dragDrop = () => otherTile = this;
+const dragOver = () => e.preventDefault();
+const dragEnter = () => e.preventDefault();
+const dragLeave = () => e.preventDefault();
+
+const dragEnd = () => {
+    // check coords of both tiles
+    let currCoords = currTile.id.split('-');
+    let r1 = parseInt(currCoords[0]);
+    let c1 = parseInt(currCoords[1]);
+    let otherCoords = otherTile.id.split('-');
+    let r2 = parseInt(otherCoords[0]);
+    let c2 = parseInt(otherCoords[1]);
+    // set valid movements (since they have to be adjacent tiles)
+    let moveLeft = c2 == c1 - 1 && r1 == r2;
+    let moveRight = c2 == c1 + 1 && r1 == r2;
+    let moveUp = r2 == r1 - 1 && c1 == c2;
+    let moveDown = r2 == r1 + 1 && c1 == c2;
+    let isAdjacent = moveLeft || moveRight || moveDown || moveUp;
+    if (isAdjacent) {
+        let currImg = currTile.src;
+        let otherImg = otherTile.src;
+        currTile.src = otherImg;
+        otherTile.src = currImg;
+    }
+}
